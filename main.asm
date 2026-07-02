2348bdf600ff2a2ea872fc5fb535dd9e13f80e8f
Caio Vieira <caioviier@gmail.com>
2026-07-01 23:40:43 -0300

    feat: emprestar e devolver

--- main.asm
+++ main.asm
@@ -2,10 +2,17 @@
     msginicial: .asciiz "Escolha entre:\n1. Cadastrar livro;\n2. Emprestar livro;\n3. Devolver livro;\n4. Listar livros;\n0. Sair;\n\nSua opção: "
     msgopcaoinvalida: .asciiz "Opção inválida!\n"
     msgcadastrar: .asciiz "Digite o titulo do livro: "
+    msg_biblioteca_cheia: .asciiz "Biblioteca cheia!\n"    
     msg_codigo: .asciiz "Codigo: "
     msglivrocadastrado: .asciiz "  -  Livro cadastrado com sucesso!\n"
     msg_disponivel: .asciiz " | Disponivel\n"
     msg_emprestado: .asciiz " | Emprestado\n"
+    msg_pedir_codigo: .asciiz "Digite o codigo do livro: "
+    msg_codigo_invalido: .asciiz "Codigo invalido!\n"
+    msg_emprestimo_ok: .asciiz "Livro emprestado com sucesso!\n"
+    msg_emprestimo_erro: .asciiz "Livro ja esta emprestado!\n"
+    msg_devolucao_ok: .asciiz "Livro devolvido com sucesso!\n"
+    msg_devolucao_erro: .asciiz "Livro nao esta emprestado!\n"
     livro_buffer: .space 100
 
     qtd_livros: .word 0
@@ -25,8 +32,8 @@
 
         beq $t0, 0, exit
         beq $t0, 1, cadastrar
-        #beq $t0, 2, emprestar
-        #beq $t0, 3, devolver
+        beq $t0, 2, emprestar
+        beq $t0, 3, devolver
         beq $t0, 4, listar
         j opcaoinvalida
 
@@ -41,6 +48,10 @@
         syscall
 
     cadastrar:
+        #verifica se a biblioteca esta cheia
+        lw $t1, qtd_livros
+        bge $t1, 50, biblioteca_cheia
+        
         #print msg
         li $v0, 4
         la $a0, msgcadastrar
@@ -127,3 +138,91 @@
     listar_proximo:
         addi $t1, $t1, 1
         j listar_loop
+
+    emprestar:
+        #print msg
+        li $v0, 4
+        la $a0, msg_pedir_codigo
+        syscall
+
+        #le o codigo do livro
+        li $v0, 5
+        syscall
+        move $t0, $v0
+
+        #valida o codigo (1 ate qtd_livros)
+        lw $t1, qtd_livros
+        blt $t0, 1, codigoinvalido
+        bgt $t0, $t1, codigoinvalido
+
+        #pega &status[codigo - 1]
+        addi $t2, $t0, -1
+        la $t3, status
+        add $t3, $t3, $t2
+        lb $t4, 0($t3)
+
+        #so empresta se estiver disponivel
+        bne $t4, 1, emprestar_erro
+        li $t5, 0
+        sb $t5, 0($t3)     # status[indice] = emprestado
+
+        li $v0, 4
+        la $a0, msg_emprestimo_ok
+        syscall
+        j menu
+
+    emprestar_erro:
+        li $v0, 4
+        la $a0, msg_emprestimo_erro
+        syscall
+        j menu
+
+    devolver:
+        #print msg
+        li $v0, 4
+        la $a0, msg_pedir_codigo
+        syscall
+
+        #le o codigo do livro
+        li $v0, 5
+        syscall
+        move $t0, $v0
+
+        #valida o codigo (1 ate qtd_livros)
+        lw $t1, qtd_livros
+        blt $t0, 1, codigoinvalido
+        bgt $t0, $t1, codigoinvalido
+
+        #pega &status[codigo - 1]
+        addi $t2, $t0, -1
+        la $t3, status
+        add $t3, $t3, $t2
+        lb $t4, 0($t3)
+
+        #so devolve se estiver emprestado
+        bne $t4, 2, devolver_erro
+        li $t5, 1
+        sb $t5, 0($t3)     # status[indice] = disponivel
+
+        li $v0, 4
+        la $a0, msg_devolucao_ok
+        syscall
+        j menu
+
+    devolver_erro:
+        li $v0, 4
+        la $a0, msg_devolucao_erro
+        syscall
+        j menu
+
+    codigoinvalido:
+        li $v0, 4
+        la $a0, msg_codigo_invalido
+        syscall
+        j menu
+
+    biblioteca_cheia:
+        li $v0, 4
+        la $a0, msg_biblioteca_cheia
+        syscall
+        j menu
